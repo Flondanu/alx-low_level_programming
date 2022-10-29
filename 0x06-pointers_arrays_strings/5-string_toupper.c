3b571012e06e6e3392db40293777ac6750100146
Flondanu <florencendanu82@gmail.com>
2022-10-29 22:11:58 +0000

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,7 +13,7 @@ int count = 0;
 
 while (*(s + count) != '\0')
 {
-if (((s + count) >= 97) && ((s + count) <= 122))
+if ((*(s + count) >= 97) && (*(s + count) <=  122))
 *(s + count) = *(s + count) - 32;
 count++;
 }
