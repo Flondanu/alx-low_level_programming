0bb05bdddb065c547f426b83399dbabb13444ae2
Flondanu <florencendanu82@gmail.com>
2022-11-26 17:15:48 +0000

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,12 @@
 */
 void _puts_recursion(char *s)
 {
-s = "lpa\0";
-if (*s == '\0')
+if (*s)
 {
-_putchar('\n');
-return;
-}
 _putchar(*s);
-s++;
-_puts_recursion(s);
+_puts_recursion(++s);
+}
+else
+_putchar('\n');
 }
 
