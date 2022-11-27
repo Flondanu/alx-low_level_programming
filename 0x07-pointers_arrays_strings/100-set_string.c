18b90a04a2119f999729d16f8f043343a347f741
Flondanu <florencendanu82@gmail.com>
2022-11-27 22:08:40 +0000

    Task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,18 +1,12 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
-   * print_diagsums - prints diagonal summaries
-    * @a: array to use
-     * @size: size of diagonal
-*/
-void print_diagsums(int *a, int size)
+   * set_string - Entry point
+    * @s: input
+     * @to: input
+      * Return: Always 0 (Success)
+ */
+void set_string(char **s, char *to)
 {
-int i, n, total1 = 0, total2 = 0;
-
-for (i = 0; i <= (size * size); i = i + size + 1)
-total1 = total1 + a[i];
-
-for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
-total2 = total2 + a[n];
-printf("%d, %d\n", total1, total2);
+*s = to;
 }
