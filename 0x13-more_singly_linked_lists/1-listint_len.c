b4617e0d34f4997960a59db32ce5863a7430fa67
Flondanu <florencendanu82@gmail.com>
2023-06-12 07:42:15 +0000

    2-list length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,10 +2,10 @@
 #include "lists.h"
 
 /**
-		    * listint_len - Counts the elements in a linked list
-		      * @h: The head of the linked list
-		        *
-			  * Return: The number of elements in a linked list
+* listint_len - Counts the elements in a linked list
+* @h: The head of the linked list
+*
+* Return: The number of elements in a linked list
 */
 size_t listint_len(const listint_t *h)
 {
